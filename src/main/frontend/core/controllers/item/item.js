c96d208122cd921ec816494daa0e56763140af86
Matthew Bolanos <mbolanos@researchnow.com>
2015-10-03 15:53:44 -0500

    Just a bit of refactoring because I want to be cool.

--- src/main/frontend/core/controllers/item/item.js
+++ src/main/frontend/core/controllers/item/item.js
@@ -2,20 +2,16 @@ class ItemCtrl {
 
     constructor($scope, $stateParams, $state, ApiService){
     	var self = this;
-    	this._$state = $state;
-    	this._$stateParams = $stateParams;
-    	this._ApiService = ApiService;
 
     	ApiService.getItem($stateParams.id).then(function(data) {
-    		self.item = data.data;
+    		$scope.item = data.data;
     	});
-    }
 
-    save() {
-    	var self = this;
-    	this._ApiService.updateItem(this._$stateParams.id, self.item).then(function(data) {
-    		self._$state.go("container.items");
-    	});
+        $scope.save = function() {
+            ApiService.updateItem($stateParams.id, $scope.item).then(function(data) {
+                $state.go("container.items");
+            });
+        }
     }
 }
 
