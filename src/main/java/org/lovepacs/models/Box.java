52aa83530e3c7adf0cd47e2041ae1edd0b796215
Tim Sporcic <tsporcic@RSNPLALTM30.local>
2015-10-02 20:29:49 -0500

    Added the Box and BoxRepository

---
+++ src/main/java/org/lovepacs/models/Box.java
@@ -0,0 +1,47 @@
+package org.lovepacs.models;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+/**
+ * @version 1.0
+ */
+@Entity
+@Table(name = "boxes")
+public class Box {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @NotNull
+    private String name;
+
+    @NotNull
+    private Boolean enabled;
+
+    public Box(String name, Boolean enabled) {
+        this.name = name;
+        this.enabled = enabled;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Boolean isEnabled() {
+        return enabled;
+    }
+
+    public void setEnabled(Boolean enabled) {
+        this.enabled = enabled;
+    }
+}
