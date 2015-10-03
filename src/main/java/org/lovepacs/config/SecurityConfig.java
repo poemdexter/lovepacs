8f4d052fdb3abf306931fd223ebede88cdf1f19d
Daniel Fairley <dfairley@researchnow.com>
2015-10-03 15:14:53 -0500

    added ability to get inventory needs publically

--- src/main/java/org/lovepacs/config/SecurityConfig.java
+++ src/main/java/org/lovepacs/config/SecurityConfig.java
@@ -3,7 +3,6 @@ package org.lovepacs.config;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
@@ -28,7 +27,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Override
     public void configure(WebSecurity web) throws Exception {
-        web.ignoring().antMatchers("/resources/**", "/**"); //todo: remove "/**"
+        web.ignoring().antMatchers("/resources/**", "/location/inventory/");
     }
 
     @Override
--- src/main/java/org/lovepacs/controllers/LocationController.java
+++ src/main/java/org/lovepacs/controllers/LocationController.java
@@ -1,6 +1,14 @@
 package org.lovepacs.controllers;
 
+import org.lovepacs.json.InventoryJson;
+import org.lovepacs.json.InventoryNeedJson;
+import org.lovepacs.json.ItemNeedJson;
+import org.lovepacs.json.LocationJson;
+import org.lovepacs.models.Inventory;
+import org.lovepacs.models.Item;
 import org.lovepacs.models.Location;
+import org.lovepacs.repositories.InventoryRepository;
+import org.lovepacs.repositories.ItemRepository;
 import org.lovepacs.repositories.LocationRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -8,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
@@ -17,6 +26,12 @@ public class LocationController {
     @Autowired
     LocationRepository locationRepository;
 
+    @Autowired
+    InventoryRepository inventoryRepository;
+
+    @Autowired
+    ItemRepository itemRepository;
+
     @RequestMapping(value = "/{id}", method = RequestMethod.GET)
     Location getLocation(@PathVariable("id") final int id) {
         return locationRepository.findOne(id);
@@ -25,6 +40,28 @@ public class LocationController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     List<Location> getAllLocations() {
         return (List<Location>)locationRepository.findAll();
+    }
+
+    @RequestMapping(value = "/inventory", method = RequestMethod.GET)
+    List<InventoryNeedJson> getAllLocationInventories() {
+        List<InventoryNeedJson> inventoryNeedList = new ArrayList<>();
+
+        List<Location> locations = (List<Location>) locationRepository.findAll();
+        for (Location location : locations) {
+            InventoryNeedJson inventoryNeed = new InventoryNeedJson();
+            inventoryNeed.setName(location.getName());
+
+            List<ItemNeedJson> itemNeedList = new ArrayList<>();
+            List<Inventory> inventories = inventoryRepository.findAllByLocationId(location.getId());
+            for (Inventory inventory : inventories) {
+                Item item = itemRepository.findOne(inventory.getItemId());
+                itemNeedList.add(new ItemNeedJson(item.getName(), inventory.getQuantity()));
+            }
+            inventoryNeed.setItems(itemNeedList);
+            inventoryNeedList.add(inventoryNeed);
+        }
+
+        return inventoryNeedList;
     }
 
     @RequestMapping(value = "/{id}", method = RequestMethod.DELETE)
