4e9acecde99a82b88fb8fefe66de7b51a1c0f073
Daniel Fairley <dfairley@researchnow.com>
2015-10-04 15:13:31 -0500

    fixed transactional for real for real for real.

--- src/main/java/org/lovepacs/repositories/InventoryRepository.java
+++ src/main/java/org/lovepacs/repositories/InventoryRepository.java
@@ -4,6 +4,7 @@ import org.lovepacs.models.Inventory;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -11,7 +12,7 @@ public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
 
     List<Inventory> findAllByLocationId(Integer locationId);
 
-    @Modifying
+    @Modifying @Transactional
     @Query("update Inventory i set i.quantity = ?3 where i.locationId = ?1 and i.itemId = ?2")
     int updateInventoryQuantities(Integer locationId, Integer itemId, Integer quantity);
 }
