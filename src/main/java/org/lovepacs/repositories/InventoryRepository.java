783faceeaf5f2ca4a262806d6122e372a389ab27
Daniel Fairley <dfairley@researchnow.com>
2015-10-04 15:09:22 -0500

    actually fixed inventory bug.

--- src/main/java/org/lovepacs/repositories/InventoryRepository.java
+++ src/main/java/org/lovepacs/repositories/InventoryRepository.java
@@ -1,17 +1,17 @@
 package org.lovepacs.repositories;
 
 import org.lovepacs.models.Inventory;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface InventoryRepository extends CrudRepository<Inventory, Integer> {
+public interface InventoryRepository extends JpaRepository<Inventory, Integer> {
 
     List<Inventory> findAllByLocationId(Integer locationId);
 
     @Modifying
-    @Query("update inventory i set i.quantity = ?3 where i.location = ?1 and i.item = ?2")
+    @Query("update Inventory i set i.quantity = ?3 where i.locationId = ?1 and i.itemId = ?2")
     Long updateInventoryQuantities(Integer locationId, Integer itemId, Integer quantity);
 }
